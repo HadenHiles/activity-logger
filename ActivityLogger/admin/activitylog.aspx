f32fe09700085fc421cd29a0eb9ce28ae2f99126
justinsearle <justin.searle@sympatico.ca>
2015-07-26 21:54:09 -0400

    Made activity log grid view page

--- ActivityLogger/admin/activitylog.aspx
+++ ActivityLogger/admin/activitylog.aspx
@@ -2,4 +2,18 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <h2>My Activities</h2>
+    <asp:Label id="lblMessage" cssClass="lblMessage" runat="server" Visible="false" Text="No Activites to show :("></asp:Label>
+    <asp:GridView ID="grdActivities" runat="server" CssClass="table table-striped" 
+        AutoGenerateColumns="false" OnRowDeleting="grdActivities_RowDeleting" DataKeyNames="id">
+        <Columns>
+            <asp:BoundField DataField="id" HeaderText="Activity ID" />
+            <asp:BoundField DataField="activity_name" HeaderText="Activity" />
+            <asp:BoundField DataField="account_id" HeaderText="User" />
+            <asp:BoundField DataField="duration" HeaderText="Duration" />
+            <asp:HyperLinkField HeaderText="Edit" NavigateUrl="~/activity-manage.aspx" Text="Edit"
+                DataNavigateUrlFields="id" DataNavigateUrlFormatString="activitylog.aspx?ActivityID={0}" />
+            <asp:CommandField HeaderText="Delete" DeleteText="Delete" ShowDeleteButton="true" />
+        </Columns>
+    </asp:GridView>
 </asp:Content>
--- ActivityLogger/admin/activitylog.aspx.cs
+++ ActivityLogger/admin/activitylog.aspx.cs
@@ -5,13 +5,60 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//references
+using ActivityLogger.Models;
+using System.Web.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace ActivityLogger.admin
 {
     public partial class activitylog : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
+        {            
+            GetActivities();
+        }
 
+        protected void GetActivities()
+        {
+            //try
+            //{
+                using (DefaultConnectionAL conn = new DefaultConnectionAL())
+                {
+                    var user = HttpContext.Current.GetOwinContext().Authentication.User.Identity.GetUserId();
+                    //use link to query the Departments model
+                    var activityItems = (from a in conn.activities
+                            where a.account_id == user
+                            select new { a.id, a.activity_name, a.account_id, a.duration }
+                    );
+                    
+                    //bind the query result to the gridview
+                    //String sort = Session["SortColumn"] + " " + Session["SortDirection"].ToString();
+                    //grdActivities.DataSource = activity.AsQueryable().OrderBy(sort).ToList();
+                    grdActivities.DataSource = activityItems.ToList();
+
+                    if (grdActivities.DataSourceObject == null)
+                    {
+                        lblMessage.Visible = true;
+                    }
+                    else
+                    {
+                        lblMessage.Visible = false;
+                    }
+
+                    grdActivities.DataBind();
+                }
+            //}
+            //catch (Exception e1)
+            //{
+                //Response.Redirect("/error.aspx");
+            //}
+        }
+        protected void grdActivities_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            
         }
     }
 }
